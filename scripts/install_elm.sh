762ede75b587e52a7f29b003ce4c560b07b55c5e
gianlo <gianlo@gmail.com>
2019-10-25 21:51:55 +0100

    use script to install elm compiler

---
+++ scripts/install_elm.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ELM_VERSION=0.19.0
+
+echo Installing elm version $ELM_VERSION
+curl -L -o elm.gz https://github.com/elm/compiler/releases/download/$ELM_VERSION/binary-for-linux-64-bit.gz
+gunzip elm.gz
+chmod +x elm
+sudo mv elm /usr/local/bin
+echo Elm compiler installed successfully
