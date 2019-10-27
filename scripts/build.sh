788eea14738051dd18c383f23015c8e53f03c673
gianlo <gianlo@gmail.com>
2019-10-27 16:59:38 +0000

    smaller assets

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SOURCE=$1
+TARGET=$2
+
+echo Building \'$SOURCE\' to \'$TARGET\'
+elm make $SOURCE --optimize --output=$TARGET
+echo Uglifying \'$TARGET\'
+npx uglify-js $TARGET --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters=true,keep_fargs=false,unsafe_comps=true,unsafe=true,passes=2' --output=$TARGET
+npx uglifyjs $TARGET --mangle --output=$TARGET
+echo Gzipping \'$TARGET\'
+gzip $TARGET
+mv ${TARGET}.gz $TARGET
+
